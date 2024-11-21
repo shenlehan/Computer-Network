63dc76e63b5b2ddcd13f370fbed1f588f8f0d73d
shenlehan <shenlehan233@gmail.com>
2024-11-21 10:39:27 +0800

    2024/11/21 Update

--- TCP Socket Programming/TCPServer.py
+++ TCP Socket Programming/TCPServer.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     serverPort = 12000
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind(('', serverPort))
-    serverSocket.listen(1)
+    serverSocket.listen(1) # get ready to receive the connetion request from the client
     print('The server is ready to receive')
 
     while True:
--- TCP Socket Programming/UDPServer.py
+++ TCP Socket Programming/UDPServer.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
     serverSocket = socket(AF_INET, SOCK_DGRAM)
     serverSocket.bind(('', serverPort))
     print('The server is ready to receive')
+    
     while True:
         message, clientAddress = serverSocket.recvfrom(2048)
         modifiedMessage = message.decode().upper()
